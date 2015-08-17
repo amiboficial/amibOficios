34e0de04e5e3ab6c6ad37f643ec4092a7ab467ca
gabriel-medina <gabriel.m.g.245245@gmail.com>
2015-08-16 20:44:00 -0500

    DEV
    desc: se agregan nuevas consultas a OficiosCNBV

--- src/main/java/mx/amib/sistemas/oficios/oficioCnbv/dao/OficioCnbvDAO.java
+++ src/main/java/mx/amib/sistemas/oficios/oficioCnbv/dao/OficioCnbvDAO.java
@@ -1,5 +1,6 @@
 package mx.amib.sistemas.oficios.oficioCnbv.dao;
 
+import java.util.Date;
 import java.util.Set;
 
 import mx.amib.sistemas.utils.SearchResult;
@@ -12,6 +13,12 @@ public interface OficioCnbvDAO {
 			Integer fechaAlDia, Integer fechaAlMes, Integer fechaAlAnio);
 	public SearchResult<OficioCnbv> findAllByIdCertificacionInAutorizados(Integer max, Integer offset, String sort, String order, Long id);
 	public SearchResult<OficioCnbv> findAllByMultipleIdCertificacionInAutorizados(Integer max, Integer offset, String sort, String order, Set<Long> multipleIds);
+	public SearchResult<OficioCnbv> findAllByNumeroOficio(Integer numeroOficio);
+	public SearchResult<OficioCnbv> findAllByClaveDga(String claveDga);
+	public SearchResult<OficioCnbv> findAllByFechaOficio(Integer max, Integer offset, String sort, String order,
+			Date fechaOficioDel, Date fechaOficioAl);
+	public boolean checkUniqueClaveDga(String claveDga);
+	public boolean checkUniqueNumeroOficio(int numeroOficio);
 	public OficioCnbv get(Long id);
 	public OficioCnbv save(OficioCnbv of);
 	public OficioCnbv update(OficioCnbv of);
--- src/main/java/mx/amib/sistemas/oficios/oficioCnbv/service/OficioCnbvService.java
+++ src/main/java/mx/amib/sistemas/oficios/oficioCnbv/service/OficioCnbvService.java
@@ -1,5 +1,6 @@
 package mx.amib.sistemas.oficios.oficioCnbv.service;
 
+import java.util.Date;
 import java.util.Set;
 
 import mx.amib.sistemas.external.oficios.oficioCnbv.OficioCnbvTO;
@@ -12,6 +13,11 @@ public interface OficioCnbvService {
 			Integer fechaAlDia, Integer fechaAlMes, Integer fechaAlAnio);
 	public SearchResult<OficioCnbvTO> findAllByIdCertificacionInAutorizados(Integer max, Integer offset, String sort, String order, Long id);
 	public SearchResult<OficioCnbvTO> findAllByMultipleIdCertificacionInAutorizados(Integer max, Integer offset, String sort, String order, Set<Long> multipleIds);
+	public SearchResult<OficioCnbvTO> findAllByNumeroOficio(Integer numeroOficio);
+	public SearchResult<OficioCnbvTO> findAllByClaveDga(String claveDga);
+	public SearchResult<OficioCnbvTO> findAllByFechaOficio(Integer max, Integer offset, String sort, String order, Date fechaOficioDel, Date fechaOficioAl);
+	public boolean checkUniqueClaveDga(String claveDga);
+	public boolean checkUniqueNumeroOficio(int numeroOficio);
 	public OficioCnbvTO get(Long id);
 	public OficioCnbvTO save(OficioCnbvTO o);
 	public OficioCnbvTO update(OficioCnbvTO o);
